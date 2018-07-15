bcd4290081b0e95234ae47f772a6f76281022379
codefii <admin@codefii.com>
2018-07-15 13:23:15 +0100

    index.php updated

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
-// spl_autoload_register(function ($class) {
-//     $root = dirname(__DIR__);   // get the parent directory
-//     $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-//     if (is_readable($file)) {
-//         require $root . '/' . str_replace('\\', '/', $class) . '.php';
-//     }
-// });
 
 require 'vendor/autoload.php';
-require_once 'vendor/codefii/framework/src/Network/Debugger/Error.php';
+require_once 'vendor/codefii/core/src/Network/Debugger/Error.php';
         \php_error\reportErrors();
 require_once 'App/Routes/Routes.php';
-// var_dump(dirname(dirname(__FILE__)));
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
