b4ec5f57c15138a5ccbea145591482bdb35994c3
Prince E. Darlington <ekeminyd@gmail.com>
2020-04-13 09:50:39 +0100

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$baseRoot = getcwd();
-$baseLoader = require $baseRoot.'/vendor/autoload.php';
-$baseLoader = require $baseRoot.'/App/routes/Routes.php';
-$baseLoader = require_once 'vendor/codefii/core/src/Network/Debugger/Error.php';
-        \php_error\reportErrors();
-$router->run();
-
